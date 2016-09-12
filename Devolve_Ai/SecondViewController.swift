537235c05d6f4e4588df81bf99b108986d45eefa
Delson Silveira <delson.silveira@kongros.com>
2016-09-12 15:24:15 -0300

    Initial Commit

---
+++ Devolve_Ai/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Devolve_Ai
+//
+//  Created by Delson Silveira on 9/12/16.
+//  Copyright © 2016 Kongros Interactive. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
